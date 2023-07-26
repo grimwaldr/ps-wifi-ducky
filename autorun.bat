672e88dbfbebb1bee5e5fbe16c42ad3ccf2411a3
Grimwald <118697420+grimwaldr@users.noreply.github.com>
2023-07-26 14:38:06 -0400

    Update autorun.bat
    removed REM line, condensed echo to one line

--- autorun.bat
+++ autorun.bat
@@ -4,13 +4,11 @@ setlocal
 rem Check for PowerShell
 where /q powershell.exe
 if %errorlevel% neq 0 (
-    echo PowerShell is not available on this computer.
-    echo How are you on a Windows Machine and you don't have PowerShell?
+    echo How are you on a Windows Machine and you don't have PowerShell? It's 2023!
     pause
     exit /b
 )
 
-rem Execute the PowerShell script
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0networks.ps1"
 
 :end
